d63e61bc0f39a24a17db1e319ba68ce6045ae8fa
ricarthlima <rrsl@cin.ufpe.br>
2022-07-31 02:22:26 -0300

    v1.3 - arrumando os temas

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'screens/home_screen/home_screen.dart';
 
 void main() {
@@ -14,6 +15,15 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.grey,
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          backgroundColor: Colors.black,
+          titleTextStyle: TextStyle(color: Colors.white),
+        ),
+        textTheme: GoogleFonts.bitterTextTheme(),
+      ),
       initialRoute: "home",
       routes: {
         "home": (context) => const HomeScreen(),
