195ceac2bd2f34a750002c530cbdaec5cb9971e4
ricarthlima <rrsl@cin.ufpe.br>
2022-07-31 21:19:28 -0300

    v3.2 - corrigido diretorio

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_webapi_first_course/screens/home_screen/add_journal_screen/add_journal_screen.dart';
 import 'package:flutter_webapi_first_course/services/journal_service.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'models/journal.dart';
+import 'screens/add_journal_screen/add_journal_screen.dart';
 import 'screens/home_screen/home_screen.dart';
 
 void main() {
